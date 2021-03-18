5cbef3fbfe4d9408efff555cd25f08d6643f47bc
KatsiarynaMolochko <katsyarina.molochko@gmail.com>
2021-03-18 09:54:44 +0300

    string

---
+++ cmake-build-debug/CMakeFiles/ClassString.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassString.dir/main.cpp.obj"
+  "ClassString.exe"
+  "ClassString.exe.manifest"
+  "ClassString.pdb"
+  "libClassString.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
